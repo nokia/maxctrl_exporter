f84fe984782c03a1bd401358e4232216f1ada59b
saleh <amgad.saleh@nokia.com>
2026-02-09 16:35:23 -0600

    add support for 25.x releases
    Signed-off-by: saleh <amgad.saleh@nokia.com>

--- auto_failover_value.go
+++ auto_failover_value.go
@@ -19,13 +19,10 @@ import (
 	"strings"
 )
 
-// AutoFailoverValue is a custom type that can handle both bool (true/false) and string values
-// String values are expected to be one of: "true", "false", or "safe" 
-// This is to support all maxscale versions: in addition to pre-25.x all 25.x new releases
+// AutoFailoverValue is a custom type that can handle both bool and string values
+// String values are expected to be one of: "true", "false", or "safe"
 type AutoFailoverValue struct {
-	StringValue string
-	BoolValue   bool
-	IsString    bool
+	BoolValue bool
 }
 
 // UnmarshalJSON implements the json.Unmarshaler interface
@@ -34,8 +31,6 @@ func (a *AutoFailoverValue) UnmarshalJSON(data []byte) error {
 	var boolValue bool
 	if err := json.Unmarshal(data, &boolValue); err == nil {
 		a.BoolValue = boolValue
-		a.StringValue = ""
-		a.IsString = false
 		return nil
 	}
 
@@ -44,18 +39,9 @@ func (a *AutoFailoverValue) UnmarshalJSON(data []byte) error {
 	if err := json.Unmarshal(data, &stringValue); err == nil {
 		// Validate that the string is one of the expected enum values
 		lowerValue := strings.ToLower(stringValue)
-		if lowerValue == "true" || lowerValue == "false" || lowerValue == "safe" {
-			a.StringValue = stringValue
-			a.BoolValue = false
-			a.IsString = true
-			return nil
-		} else {
-			// If not a valid enum value, default to "false"
-			a.StringValue = "false"
-			a.BoolValue = false
-			a.IsString = true
-			return nil
-		}
+		// Set BoolValue to true for "true" or "safe", false otherwise
+		a.BoolValue = lowerValue == "true" || lowerValue == "safe"
+		return nil
 	}
 
 	// If neither, return error
@@ -64,31 +50,10 @@ func (a *AutoFailoverValue) UnmarshalJSON(data []byte) error {
 
 // MarshalJSON implements the json.Marshaler interface
 func (a AutoFailoverValue) MarshalJSON() ([]byte, error) {
-	if a.IsString {
-		return json.Marshal(a.StringValue)
-	}
 	return json.Marshal(a.BoolValue)
 }
 
-// String returns the string representation
-// For boolean values: true becomes "true", false becomes "false"
-// For string values: returns the original string value ("true", "false", or "safe")
-func (a AutoFailoverValue) String() string {
-	if a.IsString {
-		return a.StringValue
-	}
-	if a.BoolValue {
-		return "true"
-	}
-	return "false"
-}
-
 // Bool returns the boolean value
-// For string values: "true" returns true, "safe" returns true, all others return false
 func (a AutoFailoverValue) Bool() bool {
-	if a.IsString {
-		s := strings.ToLower(a.StringValue)
-		return s == "true" || s == "safe"
-	}
 	return a.BoolValue
 }
